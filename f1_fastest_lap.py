a7c6778cf6981b4f82ffedab705902123b65864f
Vinh Hoang <vinh.donhoang@gmail.com>
2025-11-15 00:57:27 -0800

    Initial commit of multiple files

--- f1_fastest_lap.py
+++ f1_fastest_lap.py
@@ -1,47 +1,57 @@
 import fastf1 as f1
 from fastf1 import plotting
+from fastf1.plotting import get_driver_style
 from matplotlib import pyplot as plt
 f1.Cache.enable_cache('C:/Users/Admin/OneDrive/Documents/Coding/Python/Learning_data_science/cache')
 
+# Enable Matplotlib patches for plotting timedelta values and load
+# FastF1's dark color scheme
+plotting.setup_mpl(
+    mpl_timedelta_support=True,
+    color_scheme='fastf1'
+)
+
 #loading the session
-session = f1.get_session(2025,'Netherlands','Race')
-telemetary = session.load()
+session = f1.get_session(2025,'Monza','Race')
+session.load()
 
 #Race results
 result = session.results
 
 #Filtering the data
-filtered_result = result.loc[:,['BroadcastName','DriverNumber','Abbreviation','FirstName','TeamName','Position','Status']]
+filtered_result = result.loc[:,['BroadcastName','Abbreviation','Position','TeamName']]
 
-#Top 3 of the race
-top3 = filtered_result.head(3)
-top3['Position'] = top3['Position'].astype(int)
-
-
-#Rename columns
-top3.rename(columns={
-    'BroadcastName' : 'Driver',
-    'DriverNumber' : 'Driver.No'
-}, inplace=True)
-
+#Top 10 of the race
+top10 = filtered_result.head(10)
+top10['Position'] = top10['Position'].astype(int)
 
 #Plot the data
-colors = ['orange','blue','cyan']
+plt.figure(figsize=(12,6))
 
-plt.figure(figsize=(10,6))
+for i, (_, row) in enumerate(top10.iterrows()):
+    abv = row['Abbreviation']
 
-for i, (_, row) in enumerate(top3.iterrows()):
-    drv_no = row['Abbreviation']
-    driver_laps = session.laps.pick_driver(drv_no)
-    fast_lap = driver_laps.pick_fastest()
-    tel = fast_lap.get_telemetry()
+    style = get_driver_style(abv, style=['color', 'linestyle'] ,session=session)
 
-    plt.plot(tel['Distance'], tel['Speed'], label=drv_no, color=colors[i])
+    drv_laps = session.laps.pick_driver(abv).copy()
+    # Convert lap times to seconds
+    drv_laps['LapTimeSeconds'] = drv_laps['LapTime'].dt.total_seconds()
 
+    #Filter pitstops
+    clean_laps = drv_laps[drv_laps['PitInTime'].isna() &
+                      drv_laps['PitOutTime'].isna()
+                      ]
+    
+    plt.plot(
+        clean_laps['LapNumber'],
+        clean_laps['LapTimeSeconds'],
+        label=row['BroadcastName'],
+        **style
+    )
 
-plt.title("Top 3 Drivers: Fastest Lap Speed Comparison")
-plt.xlabel("Distance (m)")
-plt.ylabel("Speed (km/h)")
+plt.title("Top 10 Drivers: Lap By Lap Race Pace ")
+plt.xlabel("Lap Number")
+plt.ylabel("Lap Time (seconds)")
 plt.legend()
 plt.grid(True)
 plt.show()
